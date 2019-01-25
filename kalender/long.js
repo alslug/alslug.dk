7885786715673853e36f9b28f1de1027d21b9413
Henning Wangerin <sparklespare-login@henning.wangerin.dk>
2019-01-25 15:19:27 +0100

    / ‘kalender/alt.ics’ + ‘kalender/long.js’ + ‘kalender/short.js’

---
+++ kalender/long.js
@@ -0,0 +1,18 @@
+
+document.getElementById('accordion').innerHTML = ''+
+
+				'<h3>L&oslash;rdag d. 9. Februar 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>Onsdag d. 20. Februar 19:00: Linuxaften</h3>'+
+				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 9. Marts 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>Tirsdag d. 19. Marts 19:00: Linuxaften</h3>'+
+				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
+				'</div>'+
+				'<h3>Mandag d. 1. April 19:00: Linuxaften</h3>'+
+				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
+				'</div>'+				'';
