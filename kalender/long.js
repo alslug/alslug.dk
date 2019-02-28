ebfe0d5f5ffae605803d61d8d658b6d4832a7c4f
Henning Wangerin <sparklespare-login@henning.wangerin.dk>
2019-02-28 20:03:07 +0100

    / ‘kalender/alt.ics’ / ‘kalender/long.js’ / ‘kalender/short.js’

--- kalender/long.js
+++ kalender/long.js
@@ -1,20 +1,26 @@
 
 document.getElementById('accordion').innerHTML = ''+
 
-				'<h3>L&oslash;rdag d. 9. Februar 13:00: Installfest</h3>'+
-				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
-				'</div>'+
-				'<h3>Onsdag d. 20. Februar 19:00: Linuxaften</h3>'+
-				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
-				'</div>'+
 				'<h3>L&oslash;rdag d. 9. Marts 13:00: Installfest</h3>'+
 				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
 				'</div>'+
 				'<h3>Tirsdag d. 19. Marts 19:00: Linuxaften</h3>'+
 				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+
+				'<h3>Fredag d. 29. Marts 19:00: Pakning til udstilling</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 30. Marts 9:00: Udstilling p&aring; Radioamat&oslash;rernes For&aring;rsmarked i Agerskov</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
 				'<h3>Mandag d. 1. April 19:00: Linuxaften</h3>'+
 				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
+				'</div>'+
+				'<h3>Onsdag d. 10. April 19:00: Info-m&oslash;de om kulturnatten (kun bestyrelsen)</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
 				'</div>'+
 				'<h3>L&oslash;rdag d. 13. April 13:00: Installfest</h3>'+
 				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
@@ -35,6 +41,10 @@ document.getElementById('accordion').innerHTML = ''+
 				'<h3>L&oslash;rdag d. 8. Juni 13:00: Installfest</h3>'+
 				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
 				'</div>'+
+				'<h3>Onsdag d. 12. Juni 19:00: Pladsfordeling til kulturnatten (kun bestyrelsen)</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
 				'<h3>Tirsdag d. 18. Juni 19:00: Linuxaften</h3>'+
 				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+
@@ -50,16 +60,10 @@ document.getElementById('accordion').innerHTML = ''+
 				'<h3>Mandag d. 19. August 19:00: Linuxaften</h3>'+
 				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+
-				'<h3>Torsdag d. 5. September 19:00: Generalforsamling</h3>'+
+				'<h3>Torsdag d. 22. August 19:00: Pakning til Kulturnatten</h3>'+
 				'<div>'+
 				'Her skal der lige lidt mere info med.'+
 				'</div>'+
-				'<h3>L&oslash;rdag d. 14. September 13:00: Installfest</h3>'+
-				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
-				'</div>'+
-				'<h3>Torsdag d. 26. September 19:00: Linuxaften</h3>'+
-				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
-				'</div>'+
-				'<h3>L&oslash;rdag d. 12. Oktober 13:00: Installfest</h3>'+
-				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'<h3>Fredag d. 23. August 17:00: Kulturnat</h3>'+
+				'<div>'+'<div>Vi udstiller på havnefronten i Sønderborg.</div>'+'<br />'+'<div>Besøg os og se hvordan en PC med linux kan se ud.</div>'+
 				'</div>'+				'';
