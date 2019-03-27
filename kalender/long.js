a6132380cf126f44fadac2e2f779224b0f9addd0
Henning Wangerin <sparklespare-login@henning.wangerin.dk>
2019-03-27 19:20:44 +0100

    / ‘kalender/alt.ics’ / ‘kalender/long.js’ / ‘kalender/short.js’

--- kalender/long.js
+++ kalender/long.js
@@ -1,12 +1,6 @@
 
 document.getElementById('accordion').innerHTML = ''+
 
-				'<h3>L&oslash;rdag d. 9. Marts 13:00: Installfest</h3>'+
-				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
-				'</div>'+
-				'<h3>Tirsdag d. 19. Marts 19:00: Linuxaften</h3>'+
-				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
-				'</div>'+
 				'<h3>Fredag d. 29. Marts 19:00: Pakning til udstilling</h3>'+
 				'<div>'+
 				'Her skal der lige lidt mere info med.'+
@@ -38,6 +32,26 @@ document.getElementById('accordion').innerHTML = ''+
 				'<h3>Onsdag d. 22. Maj 19:00: Linuxaften</h3>'+
 				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+
+				'<h3>Onsdag d. 29. Maj 17:00: Opstilling til LugCamp</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
+				'<h3>Torsdag d. 30. Maj 10:00: LugCamp</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
+				'<h3>Fredag d. 31. Maj 0:00: LugCamp</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 1. Juni 0:00: LugCamp</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
+				'<h3>S&oslash;ndag d. 2. Juni 0:00: LugCamp</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
 				'<h3>L&oslash;rdag d. 8. Juni 13:00: Installfest</h3>'+
 				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
 				'</div>'+
@@ -56,14 +70,4 @@ document.getElementById('accordion').innerHTML = ''+
 				'</div>'+
 				'<h3>L&oslash;rdag d. 10. August 13:00: Installfest</h3>'+
 				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
-				'</div>'+
-				'<h3>Mandag d. 19. August 19:00: Linuxaften</h3>'+
-				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
-				'</div>'+
-				'<h3>Torsdag d. 22. August 19:00: Pakning til Kulturnatten</h3>'+
-				'<div>'+
-				'Her skal der lige lidt mere info med.'+
-				'</div>'+
-				'<h3>Fredag d. 23. August 17:00: Kulturnat</h3>'+
-				'<div>'+'<div>Vi udstiller på havnefronten i Sønderborg.</div>'+'<br />'+'<div>Besøg os og se hvordan en PC med linux kan se ud.</div>'+
 				'</div>'+				'';
