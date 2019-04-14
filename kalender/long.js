a9d35496bb304a40cef637af39296fd6e94d8469
Henning Wangerin <sparklespare-login@henning.wangerin.dk>
2019-04-14 10:31:19 +0200

    / ‘kalender/alt.ics’ / ‘kalender/long.js’ / ‘kalender/short.js’

--- kalender/long.js
+++ kalender/long.js
@@ -1,28 +1,26 @@
 
 document.getElementById('accordion').innerHTML = ''+
 
-				'<h3>Fredag d. 29. Marts 19:00: Pakning til udstilling</h3>'+
+				'<h3>Onsdag d. 17. April 19:00: Python-gruppen</h3>'+
 				'<div>'+
 				'Her skal der lige lidt mere info med.'+
 				'</div>'+
-				'<h3>L&oslash;rdag d. 30. Marts 9:00: Udstilling p&aring; Radioamat&oslash;rernes For&aring;rsmarked i Agerskov</h3>'+
+				'<h3>Onsdag d. 24. April 19:00: Python-gruppen</h3>'+
 				'<div>'+
 				'Her skal der lige lidt mere info med.'+
-				'</div>'+
-				'<h3>Mandag d. 1. April 19:00: Linuxaften</h3>'+
-				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
-				'</div>'+
-				'<h3>Onsdag d. 10. April 19:00: Info-m&oslash;de om kulturnatten (kun bestyrelsen)</h3>'+
-				'<div>'+
-				'Her skal der lige lidt mere info med.'+
-				'</div>'+
-				'<h3>L&oslash;rdag d. 13. April 13:00: Installfest</h3>'+
-				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
 				'</div>'+
 				'<h3>Torsdag d. 25. April 19:00: Linuxaften</h3>'+
 				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+
+				'<h3>Onsdag d. 1. Maj 19:00: Python-gruppen</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
 				'<h3>Fredag d. 3. Maj 0:00: AlsLUGs f&oslash;dselsdag (19 &aring;r)</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
+				'<h3>Onsdag d. 8. Maj 19:00: Python-gruppen</h3>'+
 				'<div>'+
 				'Her skal der lige lidt mere info med.'+
 				'</div>'+
@@ -70,4 +68,7 @@ document.getElementById('accordion').innerHTML = ''+
 				'</div>'+
 				'<h3>L&oslash;rdag d. 10. August 13:00: Installfest</h3>'+
 				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>Mandag d. 19. August 19:00: Linuxaften</h3>'+
+				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+				'';
