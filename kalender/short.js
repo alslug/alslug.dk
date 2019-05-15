baf807d2d02f02cfcb6d5321aec932caea0950a7
Henning Wangerin <sparklespare-login@henning.wangerin.dk>
2019-05-15 16:21:02 +0200

    / ‘kalender/alt.ics’ / ‘kalender/long.js’ / ‘kalender/short.js’

--- kalender/short.js
+++ kalender/short.js
@@ -1,22 +1,22 @@
 
 document.getElementById('accordion').innerHTML = ''+
 
-				'<h3>Onsdag d. 17. April 19:00: Python-gruppen</h3>'+
+				'<h3>I dag 19:00: Python-gruppen</h3>'+
 				'<div>'+
 				'Her skal der lige lidt mere info med.'+
 				'</div>'+
-				'<h3>Onsdag d. 24. April 19:00: Python-gruppen</h3>'+
-				'<div>'+
-				'Her skal der lige lidt mere info med.'+
-				'</div>'+
-				'<h3>Torsdag d. 25. April 19:00: Linuxaften</h3>'+
+				'<h3>Onsdag d. 22. Maj 19:00: Linuxaften</h3>'+
 				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+
-				'<h3>Onsdag d. 1. Maj 19:00: Python-gruppen</h3>'+
+				'<h3>Torsdag d. 23. Maj 19:00: Python-gruppen</h3>'+
 				'<div>'+
 				'Her skal der lige lidt mere info med.'+
 				'</div>'+
-				'<h3>Fredag d. 3. Maj 0:00: AlsLUGs f&oslash;dselsdag (19 &aring;r)</h3>'+
+				'<h3>Onsdag d. 29. Maj 17:00: Opstilling til LugCamp</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
+				'<h3>Torsdag d. 30. Maj 10:00: LugCamp</h3>'+
 				'<div>'+
 				'Her skal der lige lidt mere info med.'+
 				'</div>'+				'';
