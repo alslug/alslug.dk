bdef69b1cabb6f7a2b8baab5aea26f0720b147e2
Henning Wangerin <sparklespare-login@henning.wangerin.dk>
2019-08-29 10:01:57 +0200

    / ‘kalender/alt.ics’ / ‘kalender/long.js’ / ‘kalender/short.js’

--- kalender/long.js
+++ kalender/long.js
@@ -1,64 +1,66 @@
 
 document.getElementById('accordion').innerHTML = ''+
 
-				'<h3>I dag 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
-				'</div>'+
-				'<h3>Onsdag d. 4. September 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
-				'</div>'+
 				'<h3>Torsdag d. 5. September 19:00: Generalforsamling</h3>'+
 				'<div>'+
 				'Her skal der lige lidt mere info med.'+
 				'</div>'+
-				'<h3>Onsdag d. 11. September 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
-				'</div>'+
 				'<h3>L&oslash;rdag d. 14. September 13:00: Installfest</h3>'+
 				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
-				'</div>'+
-				'<h3>Onsdag d. 18. September 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
-				'</div>'+
-				'<h3>Onsdag d. 25. September 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
 				'</div>'+
 				'<h3>Torsdag d. 26. September 19:00: Linuxaften</h3>'+
 				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+
-				'<h3>Onsdag d. 2. Oktober 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
-				'</div>'+
-				'<h3>Onsdag d. 9. Oktober 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
-				'</div>'+
 				'<h3>L&oslash;rdag d. 12. Oktober 13:00: Installfest</h3>'+
 				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
-				'</div>'+
-				'<h3>Onsdag d. 16. Oktober 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
 				'</div>'+
 				'<h3>Onsdag d. 23. Oktober 19:00: Linuxaften</h3>'+
 				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+
-				'<h3>Onsdag d. 30. Oktober 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
-				'</div>'+
-				'<h3>Onsdag d. 6. November 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
-				'</div>'+
 				'<h3>L&oslash;rdag d. 9. November 13:00: Installfest</h3>'+
 				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
-				'</div>'+
-				'<h3>Onsdag d. 13. November 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
 				'</div>'+
 				'<h3>Tirsdag d. 19. November 19:00: Linuxaften</h3>'+
 				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+
-				'<h3>Onsdag d. 20. November 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
+				'<h3>Onsdag d. 4. December 19:00: Linuxaften</h3>'+
+				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+
-				'<h3>Onsdag d. 27. November 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
+				'<h3>L&oslash;rdag d. 14. December 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 11. Januar 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 8. Februar 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 14. Marts 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 11. April 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>S&oslash;ndag d. 3. Maj 0:00: AlsLUGs f&oslash;dselsdag (20 &aring;r)</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 9. Maj 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 13. Juni 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 11. Juli 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 8. August 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>Torsdag d. 3. September 19:00: Generalforsamling</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 12. September 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
 				'</div>'+				'';
--- kalender/short.js
+++ kalender/short.js
@@ -1,19 +1,19 @@
 
 document.getElementById('accordion').innerHTML = ''+
 
-				'<h3>I dag 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
-				'</div>'+
-				'<h3>Onsdag d. 4. September 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
-				'</div>'+
 				'<h3>Torsdag d. 5. September 19:00: Generalforsamling</h3>'+
 				'<div>'+
 				'Her skal der lige lidt mere info med.'+
 				'</div>'+
-				'<h3>Onsdag d. 11. September 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi lærer grundlæggende Python.</div>'+'<div>Og vi vil på sigt lave nogle projekter i sproget</div>'+'<div>Evt. med en Raspberry Pi.</div>'+
-				'</div>'+
 				'<h3>L&oslash;rdag d. 14. September 13:00: Installfest</h3>'+
 				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>Torsdag d. 26. September 19:00: Linuxaften</h3>'+
+				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
+				'</div>'+
+				'<h3>L&oslash;rdag d. 12. Oktober 13:00: Installfest</h3>'+
+				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
+				'</div>'+
+				'<h3>Onsdag d. 23. Oktober 19:00: Linuxaften</h3>'+
+				'<div>'+'<div>På en linuxaften er fokus primært på at hjælpe folk, der har fået installeret linux på deres maskine til den sidste installfest, og nu har en masse spørgsmål om de nye programmer de har fået installeret.</div>'+'<br />'+'<div>Men der er altid mulighed for at få installeret linux på en maskine eller tre.</div>'+
 				'</div>'+				'';
