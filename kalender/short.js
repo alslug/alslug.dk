a122c26630ad1c685f4ab6c11c0298256cc2af5f
Henning Wangerin <github.com@henning.wangerin.dk>
2019-09-27 20:42:14 +0200

    Add files via upload

--- kalender/short.js
+++ kalender/short.js
@@ -1,19 +1,20 @@
 
 document.getElementById('accordion').innerHTML = ''+
 
-				'<h3>Onsdag d. 4. September 19:00: Python-gruppen</h3>'+
+				'<h3>Tirsdag d. 1. Oktober 19:00: Python-gruppen</h3>'+
 				'<div>'+'<div>Vi mødes primært til en at lave snak eller sparring om python og andre programmering sprog.</div>'+'<div>Vi vil fokusere på private projekter eller temaer som TKinter eller Pygame ol.</div>'+'<div>Er du ny bruger kan vi hjælpe dig med at komme videre i Python.</div>'+'<div>Er du rutineret bruger vil vi gerne høre om dine erfaringer med Python.</div>'+'<div>Der vil være rig mulighed for at lave nogle fælles projekter.</div>'+'<div>&nbsp;</div>'+'<div>PS: Programmeringen er ikke relateret til kun Linux. Så har du kun en Windows maskine så kom bare alligevel. </div>'+
 				'</div>'+
-				'<h3>Torsdag d. 5. September 19:00: Generalforsamling</h3>'+
+				'<h3>Fredag d. 4. Oktober 14:00: Opstilling</h3>'+
 				'<div>'+
 				'Her skal der lige lidt mere info med.'+
 				'</div>'+
-				'<h3>Tirsdag d. 10. September 19:00: Python-gruppen</h3>'+
+				'<h3>L&oslash;rdag d. 5. Oktober 9:00: Udstilling, Land&Fritid, Ullerup</h3>'+
+				'<div>'+
+				'Her skal der lige lidt mere info med.'+
+				'</div>'+
+				'<h3>Tirsdag d. 8. Oktober 19:00: Python-gruppen</h3>'+
 				'<div>'+'<div>Vi mødes primært til en at lave snak eller sparring om python og andre programmering sprog.</div>'+'<div>Vi vil fokusere på private projekter eller temaer som TKinter eller Pygame ol.</div>'+'<div>Er du ny bruger kan vi hjælpe dig med at komme videre i Python.</div>'+'<div>Er du rutineret bruger vil vi gerne høre om dine erfaringer med Python.</div>'+'<div>Der vil være rig mulighed for at lave nogle fælles projekter.</div>'+'<div>&nbsp;</div>'+'<div>PS: Programmeringen er ikke relateret til kun Linux. Så har du kun en Windows maskine så kom bare alligevel. </div>'+
 				'</div>'+
-				'<h3>L&oslash;rdag d. 14. September 13:00: Installfest</h3>'+
+				'<h3>L&oslash;rdag d. 12. Oktober 13:00: Installfest</h3>'+
 				'<div>'+'<div>En installfest (Installparty som er den internationale betegnelse) hjælper vi alle med at få installeret Linux på deres medbragte computere, samt hjælper med de problemer der opstår undervejs.</div>'+'<br />'+'<div>Derudover hjælper vi selvfølgelig med de problemstillinger som vore gæster kommer med.</div>'+
-				'</div>'+
-				'<h3>Tirsdag d. 17. September 19:00: Python-gruppen</h3>'+
-				'<div>'+'<div>Vi mødes primært til en at lave snak eller sparring om python og andre programmering sprog.</div>'+'<div>Vi vil fokusere på private projekter eller temaer som TKinter eller Pygame ol.</div>'+'<div>Er du ny bruger kan vi hjælpe dig med at komme videre i Python.</div>'+'<div>Er du rutineret bruger vil vi gerne høre om dine erfaringer med Python.</div>'+'<div>Der vil være rig mulighed for at lave nogle fælles projekter.</div>'+'<div>&nbsp;</div>'+'<div>PS: Programmeringen er ikke relateret til kun Linux. Så har du kun en Windows maskine så kom bare alligevel. </div>'+
 				'</div>'+				'';
